535e443f081f86625d172374730f4a8d8e8b9cdb
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2026-06-27 23:56:48 +0530

    Update llm_manager.py

--- src/llm_manager.py
+++ src/llm_manager.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from src.config import GROQ_API_KEY
+from config import GROQ_API_KEY
 
 sessions_db = {}
 
